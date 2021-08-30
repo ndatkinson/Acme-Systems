341e2db634a3b78d8504a054746400d88f940632
Noah Atkinson <ndatkinson@valdosta.edu>
2021-08-30 09:38:13 -0400

    added heyNow

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -4,5 +4,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Intellij");
         System.out.println("Hello from GitHub");
+
+    }
+    public static void heyNow() {
+        String Msg = "Hey Now!";
+        System.out.println(Msg);
     }
 }
