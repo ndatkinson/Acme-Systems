9bad193ab98d1e0524ac12e140328d6164c3e91c
Noah Atkinson <ndatkinson@valdosta.edu>
2021-08-29 21:05:17 -0400

    added print statement

--- src/ver1/HelloDolly.java
+++ src/ver1/HelloDolly.java
@@ -3,5 +3,6 @@ package ver1;
 public class HelloDolly {
     public static void main(String[] args) {
         System.out.println("Hello Dolly");
+        System.out.println("Hello Dolly from Intellij again");
     }
 }
