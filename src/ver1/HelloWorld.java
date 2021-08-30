c3955532dc557236ebdd7db05af7d89db9bf26f0
Noah Atkinson <ndatkinson@valdosta.edu>
2021-08-30 10:10:46 -0400

    fixed?

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -6,5 +6,11 @@ public class HelloWorld {
         System.out.println("Hello from GitHub");
 
     }
-
+    public static void heyNow() {
+        String Msg = "Hey Now!";
+        System.out.println(Msg);
+    }
+    public static void whatFor(String s) {
+        System.out.println(s + "stringArg, what for?");
+    }
 }
